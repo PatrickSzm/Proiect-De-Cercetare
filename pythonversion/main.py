7ec715718321953296ceca200aecf7c9cbadc2da
Szmuck Patrick Gabriel <116679298+PatrickSzm@users.noreply.github.com>
2026-01-13 23:42:37 +0200

    Add FPS measurement to Python Pygame prototype

--- pythonversion/main.py
+++ pythonversion/main.py
@@ -20,5 +20,8 @@ while running:
     pygame.draw.circle(screen, (255,0,0), (x,y), 20)
     pygame.display.flip()
     clock.tick(60)
+    fps = int(clock.get_fps())
+    fps_text = font.render(f"FPS: {fps}", True, (255,255,255))
+    screen.blit(fps_text, (10,10))
 
 pygame.quit()
